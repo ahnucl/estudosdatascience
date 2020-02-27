c208a1df093efa54ac11aa2afd74343084cca687
leonardo.cunha <leonardo.cunha@fornecedores.sicoob.com.br>
2020-02-27 18:32:50 -0300

    Alguns testes após aula teórica de Estruturas de Dados do R

---
+++ R/estrutura_de_dados.R
@@ -0,0 +1,18 @@
+X <- c(2,5,1,"sa", "asdasd",2L)
+X
+X[4] <- 1000
+X[1,1]
+
+# lembrar como carrega o data frame iris
+library(datasets)
+iris[1,1]
+iris$Species
+
+#  Estruturas de Dados em R:
+
+#  Vetores
+#  Matrizes
+#  Data Frames
+#  Listas
+#  Fatores -> variáveis categóricas
+ 
