01e0978756a7248e0d8bd83daa8483b65dc04edf
leonardo.cunha <leonardo.cunha@fornecedores.sicoob.com.br>
2020-03-05 19:40:14 -0300

    Iniciando estudo básico do Python

---
+++ Python/misc/teste_comentario.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 19:01:55 2020
+
+@author: Leonardo.Cunha
+"""
+'''
+
+isso é um comentário? Sim!
+'''
+
+x = 50000
+
+v_t = True
+
+v_f = False
+
+print(type(int(input()))) 
+
+print(type("teste")) # imprime com "<class ...>"
