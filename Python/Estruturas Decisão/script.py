55f06d7237f9d013e1d5c6a2f4859bd088f9baa3
leonardo.cunha <leonardo.cunha@fornecedores.sicoob.com.br>
2020-03-06 17:53:13 -0300

    Adicionado uns scipts para testar as estruturas básicas de decisão e repetição

---
+++ Python/Estruturas Decisão/script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 17:31:07 2020
+
+@author: Leonardo.Cunha
+"""
+
+valor = 10127
+
+if (valor > 100) :
+    print("Valor alto!")
+    if valor%2==0:
+        print("valor par")
+    else:
+        print("valor impar")
+elif valor == 100:
+    print("valor normal...");
+else:
+    print("Valor baixo!")
+    
---
+++ Python/Estruturas Repetição/script.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri Mar  6 17:41:15 2020
+
+@author: Leonardo.Cunha
+"""
+
+# sintaxe do for no python: o comum é usar range(0,x)
+
+
+help("range")
+
+for i in range(0,10,2): # terceiro parâmetro é opcional; e o incremento que sera usado
+    print(i)
+    
+for i in range(0,-10,-1): # terceiro parâmetro é opcional; e o incremento que sera usado
+    print(i)
